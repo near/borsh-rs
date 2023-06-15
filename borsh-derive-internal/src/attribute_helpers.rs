cf82828a8f4bbbc30c09fa1f68b756f299d5dd14
iho <iho@users.noreply.github.com>
2023-06-15 13:13:18 +0300

    fix fix fix

--- borsh-derive-internal/src/attribute_helpers.rs
+++ borsh-derive-internal/src/attribute_helpers.rs
@@ -5,15 +5,16 @@ pub fn contains_skip(attrs: &[Attribute]) -> bool {
 }
 
 pub fn contains_initialize_with(attrs: &[Attribute]) -> Option<Path> {
-    let mut res = None;
     for attr in attrs.iter() {
         if attr.path().is_ident("borsh_init") {
+            let mut res = None;
             let _ = attr.parse_nested_meta(|meta| {
                 res = Some(meta.path);
                 Ok(())
             });
+            return res;
         }
     }
 
-    res
+    None
 }
