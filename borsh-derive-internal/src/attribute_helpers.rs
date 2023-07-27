d02b68e7f3772d2e7f4a1bd86fedb118221e1d15
iho <iho@users.noreply.github.com>
2023-07-27 21:27:20 +0300

    Apply suggestions from code review
    Co-authored-by: dj8yf0μl <26653921+dj8yfo@users.noreply.github.com>

--- borsh-derive-internal/src/attribute_helpers.rs
+++ borsh-derive-internal/src/attribute_helpers.rs
@@ -535,9 +535,9 @@ mod tests {
             #[derive(BorshDeserialize, Debug)]
             #[borsh(use_discriminant = false)]
             #[borsh_skip]
-            struct AWithUseDiscriminantFalse {
-                x: X,
-                y: Y,
+            enum AWithUseDiscriminantFalse {
+                X,
+                Y,
             }
         })
         .unwrap();
@@ -549,9 +549,9 @@ mod tests {
         let item_enum: DeriveInput = syn::parse2(quote! {
             #[derive(BorshDeserialize, Debug)]
             #[borsh(invalid)]
-            struct AWithUseDiscriminantFalse {
-                x: X,
-                y: Y,
+            enum AWithUseDiscriminantFalse {
+                X,
+                Y,
             }
         })
         .unwrap();
