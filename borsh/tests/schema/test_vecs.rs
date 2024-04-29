238ea42994fd4f85ca03046aac1f676866492783
dj8yf0μl <26653921+dj8yfo@users.noreply.github.com>
2024-04-29 22:44:58 +0300

    feat: implement `BorshSchema` for `VecDeque` and `LinkedList` (#291)

--- borsh/tests/schema/test_vecs.rs
+++ borsh/tests/schema/test_vecs.rs
@@ -1,23 +1,34 @@
 use crate::common_macro::schema_imports::*;
+use alloc::collections::{VecDeque, LinkedList};
 
-#[test]
-fn simple_vec() {
-    let actual_name = Vec::<u64>::declaration();
-    let mut actual_defs = schema_map!();
-    Vec::<u64>::add_definitions_recursively(&mut actual_defs);
-    assert_eq!("Vec<u64>", actual_name);
-    assert_eq!(
-        schema_map! {
-            "Vec<u64>" => Definition::Sequence {
-                length_width: Definition::DEFAULT_LENGTH_WIDTH,
-                length_range: Definition::DEFAULT_LENGTH_RANGE,
-                elements: "u64".to_string(),
-            },
-            "u64" => Definition::Primitive(8)
-        },
-        actual_defs
-    );
+macro_rules! test_vec_like_collection_schema {
+    [$test_name: ident, $type: ident] => [
+
+        #[test]
+        fn $test_name() {
+            let actual_name = $type::<u64>::declaration();
+            let mut actual_defs = schema_map!();
+            $type::<u64>::add_definitions_recursively(&mut actual_defs);
+
+            assert_eq!(format!("{}<u64>", stringify!($type)), actual_name);
+            assert_eq!(
+                schema_map! {
+                    actual_name => Definition::Sequence {
+                        length_width: Definition::DEFAULT_LENGTH_WIDTH,
+                        length_range: Definition::DEFAULT_LENGTH_RANGE,
+                        elements: "u64".to_string(),
+                    },
+                    "u64" => Definition::Primitive(8)
+                },
+                actual_defs
+            );
+        }
+    ];
 }
+
+test_vec_like_collection_schema!(simple_vec, Vec);
+test_vec_like_collection_schema!(vec_deque, VecDeque);
+test_vec_like_collection_schema!(linked_list, LinkedList);
 
 #[test]
 fn nested_vec() {
